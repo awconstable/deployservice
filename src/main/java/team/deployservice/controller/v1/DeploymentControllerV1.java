16224514faf2eb9f74b1141b1ef26f3aaf634cdb
Austen Constable <awconstable@gmail.com>
2021-03-17 10:53:29 +0100

    #15 Add API documentation

--- src/main/java/team/deployservice/controller/v1/DeploymentControllerV1.java
+++ src/main/java/team/deployservice/controller/v1/DeploymentControllerV1.java
@@ -1,5 +1,8 @@
 package team.deployservice.controller.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -10,13 +13,16 @@ import team.deployservice.model.LeadTime;
 import team.deployservice.service.DeploymentService;
 
 import javax.validation.Valid;
-import java.time.*;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1", produces = "application/json")
+@Api
 public class DeploymentControllerV1
     {
     
@@ -30,42 +36,49 @@ public class DeploymentControllerV1
 
         @PostMapping("/deployment")
         @ResponseStatus(HttpStatus.CREATED)
+        @ApiOperation(value = "Store a deployment", notes = "Store a single deployment", response = Deployment.class)
         public Deployment store(@Valid @RequestBody Deployment deployment){
             return deploymentService.store(deployment);
         }
 
         @GetMapping("/deployment")
         @ResponseStatus(HttpStatus.OK)
+        @ApiOperation(value = "List all deployments", notes = "List all deployments without filtering", response = Deployment.class, responseContainer = "List")
         public List<Deployment> list(){
             return deploymentService.list();
         }
 
         @GetMapping("/deployment/{id}")
         @ResponseStatus(HttpStatus.OK)
-        public Optional<Deployment> show(@PathVariable String id){
+        @ApiOperation(value = "Get a specific deployment specified by it's id", response = Deployment.class)
+        public Optional<Deployment> show(@PathVariable @ApiParam(value = "The deployment id", required = true) String id){
             return deploymentService.get(id);
         }
     
         @DeleteMapping("/deployment/{id}")
         @ResponseStatus(HttpStatus.OK)
-        public String delete(@PathVariable String id) { return deploymentService.delete(id); }
+        @ApiOperation(value = "Delete a specific deployment specified by it's id", response = Deployment.class)
+        public String delete(@PathVariable @ApiParam(value = "The deployment id", required = true) String id) { return deploymentService.delete(id); }
 
         @GetMapping("/deployment/application/{id}")
         @ResponseStatus(HttpStatus.OK)
-        public List<Deployment> listForApp(@PathVariable String id){
+        @ApiOperation(value = "List all deployments for an application", notes = "List all deployments filtered by application id", response = Deployment.class, responseContainer = "List")
+        public List<Deployment> listForApp(@PathVariable @ApiParam(value = "The application id", required = true) String id){
             return deploymentService.listAllForApplication(id);
         }
 
         @GetMapping("/deployment/application/{id}/date/{date}")
         @ResponseStatus(HttpStatus.OK)
-        public List<Deployment> listForAppAndDate(@PathVariable String id, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+        @ApiOperation(value = "List all deployments for an application and date combination", notes = "List all deployments filtered by application id and date", response = Deployment.class, responseContainer = "List")
+        public List<Deployment> listForAppAndDate(@PathVariable @ApiParam(value = "The application id", required = true) String id, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "The deployment date in ISO Date format YYYY-MM-dd", required = true) LocalDate date){
             Date reportingDate = Date.from(date.atStartOfDay(ZoneOffset.UTC).toInstant());
             return deploymentService.listAllForApplication(id, reportingDate);
         }
 
         @GetMapping("/deployment/application/{id}/frequency")
         @ResponseStatus(HttpStatus.OK)
-        public DeploymentFrequency calculateDeployFreq(@PathVariable String id){
+        @ApiOperation(value = "Calculate deployment frequency for an application for the last 90 days", response = DeploymentFrequency.class)
+        public DeploymentFrequency calculateDeployFreq(@PathVariable @ApiParam(value = "The application id", required = true) String id){
             ZonedDateTime date = LocalDate.now().minusDays(1).atStartOfDay(ZoneOffset.UTC);        
             Date reportingDate = Date.from(date.toInstant());
             return deploymentService.calculateDeployFreq(id, reportingDate);
@@ -73,14 +86,16 @@ public class DeploymentControllerV1
     
         @GetMapping("/deployment/application/{id}/frequency/{date}")
         @ResponseStatus(HttpStatus.OK)
-        public DeploymentFrequency calculateDeployFreq(@PathVariable String id, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+        @ApiOperation(value = "Calculate deployment frequency for an application for the date specified", response = DeploymentFrequency.class)
+        public DeploymentFrequency calculateDeployFreq(@PathVariable @ApiParam(value = "The application id", required = true) String id, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "The deployment date in ISO Date format YYYY-MM-dd", required = true) LocalDate date){
             Date reportingDate = Date.from(date.atStartOfDay(ZoneOffset.UTC).toInstant());
             return deploymentService.calculateDeployFreq(id, reportingDate);
         }
 
         @GetMapping("/deployment/application/{id}/lead_time")
         @ResponseStatus(HttpStatus.OK)
-        public LeadTime calculateLeadTime(@PathVariable String id){
+        @ApiOperation(value = "Calculate lead time for an application", response = LeadTime.class)
+        public LeadTime calculateLeadTime(@PathVariable @ApiParam(value = "The application id", required = true) String id){
             ZonedDateTime date = LocalDate.now().minusDays(1).atStartOfDay(ZoneOffset.UTC);
             Date reportingDate = Date.from(date.toInstant());
             return deploymentService.calculateLeadTime(id, reportingDate);
@@ -88,7 +103,8 @@ public class DeploymentControllerV1
     
         @GetMapping("/deployment/application/{id}/lead_time/{date}")
         @ResponseStatus(HttpStatus.OK)
-        public LeadTime calculateLeadTime(@PathVariable String id, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+        @ApiOperation(value = "Calculate lead time for an application for the date specified", response = LeadTime.class)
+        public LeadTime calculateLeadTime(@PathVariable @ApiParam(value = "The application id", required = true) String id, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "The deployment date in ISO Date format YYYY-MM-dd", required = true) LocalDate date){
             Date reportingDate = Date.from(date.atStartOfDay(ZoneOffset.UTC).toInstant());
             return deploymentService.calculateLeadTime(id, reportingDate);
         }
